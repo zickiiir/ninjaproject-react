2d1421bb8346dd2073d6e264e76e54f4483fdb3f
Dominik Bláha <dominik.blaha@hvtronix.cz>
2019-12-18 14:29:30 +0100

    header component

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+
+import Header from './components/header'
 
 export default function App() {
-
+  const [todos, setTodos] = useState([
+    { text: 'buy coffee', key: '1' },
+    { text: 'create an app', key: '2' },
+    { text: 'play on the switch', key: '3' }
+  ]);
 
   return (
     <View style={styles.container}>
+      <Header />
+      <View style={styles.content}>
+        {/* to form */}
+        <View style={styles.list}>
+          <FlatList 
+            data={todos}
+            renderItem={({ item }) => (
+              <Text>{item.text}</Text>
+            )}
+          />
+        </View>
+      </View>
 
     </View>
   );
@@ -15,5 +33,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff'
+  },
+  content: {
+    padding: 40,
+  },
+  list: {
+    marginTop: 20,
   }
 });
