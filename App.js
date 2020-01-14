2ba7d46832393ff2f279303bb4ccf546b084912d
Dominik Bláha <zicko@seznam.cz>
2020-01-14 18:12:21 +0100

    done

--- App.js
+++ App.js
@@ -1,60 +1,76 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  Alert,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from 'react-native';
 
 import Header from './components/header';
 import TodoItem from './components/todoItem';
 import AddTodo from './components/addTodo';
+import Sandbox from './components/sandbox';
 
 export default function App() {
   const [todos, setTodos] = useState([
     { text: 'buy coffee', key: '1' },
     { text: 'create an app', key: '2' },
-    { text: 'play on the switch', key: '3' }
+    { text: 'play on the switch', key: '3' },
   ]);
 
-  const pressHandler = (key) => {
-    setTodos((prevTodos) => {
+  const pressHandler = key => {
+    setTodos(prevTodos => {
       return prevTodos.filter(todo => todo.key != key);
-    })
-  }
+    });
+  };
 
-  const submitHandler = (text) => {
-    setTodos((prevTodos) => {
-      return [
-        { text: text, key: Math.random().toString() },
-        ...prevTodos
-      ]
-    })
-  }
+  const submitHandler = text => {
+    if (text.length > 3) {
+      setTodos(prevTodos => {
+        return [{ text: text, key: Math.random().toString() }, ...prevTodos];
+      });
+    } else {
+      Alert.alert('OOPS', 'Todos must be over 3 characters long.', [
+        { text: 'Understood', onPress: () => console.log('Alert closed.') },
+      ]);
+    }
+  };
 
   return (
-    <View style={styles.container}>
-      <Header />
-      <View style={styles.content}>
-        <AddTodo submitHandler={submitHandler} />
-        <View style={styles.list}>
-          <FlatList 
-            data={todos}
-            renderItem={({ item }) => (
-              <TodoItem item={item} pressHandler={pressHandler} />
-            )}
-          />
+    /* <Sandbox /> */
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={styles.container}>
+        <Header />
+        <View style={styles.content}>
+          <AddTodo submitHandler={submitHandler} />
+          <View style={styles.list}>
+            <FlatList
+              data={todos}
+              renderItem={({ item }) => (
+                <TodoItem item={item} pressHandler={pressHandler} />
+              )}
+            />
+          </View>
         </View>
       </View>
-
-    </View>
+    </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff'
+    backgroundColor: '#fff',
   },
   content: {
+    flex: 1,
     padding: 40,
   },
   list: {
+    flex: 1,
     marginTop: 20,
-  }
+  },
 });
--- components/header.js
+++ components/header.js
@@ -2,23 +2,23 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function Header() {
-    return (
-        <View style={styles.header}>
-            <Text style={styles.title}>My Todos:</Text>
-        </View>
-    )
+  return (
+    <View style={styles.header}>
+      <Text style={styles.title}>My Todos:</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    header: {
-        height: 100,
-        paddingTop: 58,
-        backgroundColor: 'coral'
-    },
-    title: {
-        textAlign: 'center', 
-        color: '#fff',
-        fontSize: 20,
-        fontWeight: 'bold'
-    }
-})
+  header: {
+    height: 100,
+    paddingTop: 58,
+    backgroundColor: 'coral',
+  },
+  title: {
+    textAlign: 'center',
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
